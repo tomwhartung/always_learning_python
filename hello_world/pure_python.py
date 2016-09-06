2dcc5d6df0e2e8f817b6f039e0181d55753b4371
Tom W Hartung <tomwhartung@gmail.com>
2016-09-06 12:03:43 -0600

    Got the pure_python.py version of the hello_world project to work ok.  Be sure to see comments in this and the ../README.md files.

---
+++ hello_world/pure_python.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+# Hello world - pure python version (without using django)
+#
+# Reference:
+#   http://dfpp.readthedocs.io/en/latest/chapter_01.html
+# Also:
+#   See the ../README.md file for important information about
+#   setting up a virtual environment that includes the six library
+#
+import textwrap
+
+from six.moves.BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class HelloRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path != '/':
+            self.send_error(404, "Object not found")
+            return
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        response_text = textwrap.dedent('''\
+            <html>
+            <head>
+                <title>Greetings to the world</title>
+            </head>
+            <body>
+                <h1>Greetings to the world</h1>
+                <p>Hello, world!</p>
+            </body>
+            </html>
+        ''')
+        self.wfile.write(response_text.encode('utf-8'))
+
+
+server_address = ('', 8000)
+httpd = HTTPServer(server_address, HelloRequestHandler)
+httpd.serve_forever()
+
