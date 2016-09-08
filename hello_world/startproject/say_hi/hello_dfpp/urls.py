7a2f2215c868caeeb75b8026b7ff0a5d84826d5d
Tom W Hartung <tomwhartung@gmail.com>
2016-09-08 11:22:16 -0600

    Removed references to "patterns" because they were giving us errors.

--- hello_world/startproject/say_hi/hello_dfpp/urls.py
+++ hello_world/startproject/say_hi/hello_dfpp/urls.py
@@ -2,14 +2,17 @@
 ## From the Writing the code section of
 ##    http://dfpp.readthedocs.io/en/latest/chapter_01.html
 ##
-
-from django.conf.urls import patterns, url
+## Updated to fix import error ("cannot import name 'patterns'")
+## Reference: http://stackoverflow.com/questions/8074955/cannot-import-name-patterns
+##
+## from django.conf.urls import patterns, url
+from django.conf.urls import *
 
 from hello.views import HomePageView
 
-urlpatterns = patterns(
+urlpatterns = [
     '',
 
     url(r'^$', HomePageView.as_view(), name='home'),
-)
+]
 
