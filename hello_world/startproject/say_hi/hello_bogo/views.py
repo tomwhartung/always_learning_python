963d38c5de9571e3fa1d3f238244ca406de27ea1
Tom W Hartung <tomwhartung@gmail.com>
2016-09-08 11:56:17 -0600

    Updated views.py as per the "bogo" tutorial at http://www.bogotobogo.com/python/Django/Python_Django_hello_world.php .

--- hello_world/startproject/say_hi/hello_bogo/views.py
+++ hello_world/startproject/say_hi/hello_bogo/views.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+##
+## From Edit views.py section of
+##    http://www.bogotobogo.com/python/Django/Python_Django_hello_world.php
+##
+from django.http import HttpResponse
+def foo(request):
+    return HttpResponse("Hello World!")
+
