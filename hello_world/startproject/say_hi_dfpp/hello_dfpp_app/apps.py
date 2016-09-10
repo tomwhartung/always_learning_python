75410ff372c3cf298e6892087a715d3b16efbccc
Tom W Hartung <tomwhartung@gmail.com>
2016-09-09 18:45:16 -0600

    Initial commit of all generated (default) code for say_hi_dfpp and hello_dfpp_app.

---
+++ hello_world/startproject/say_hi_dfpp/hello_dfpp_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HelloDfppAppConfig(AppConfig):
+    name = 'hello_dfpp_app'
---
+++ hello_world/startproject/say_hi_dfpp/say_hi_dfpp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for say_hi_dfpp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "say_hi_dfpp.settings")
+
+application = get_wsgi_application()
