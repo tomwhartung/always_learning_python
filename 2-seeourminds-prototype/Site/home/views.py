8acffbf07b18d7121f3aed05ef3c4d1f7ed3a320
Tom W Hartung <tomwhartung@gmail.com>
2016-10-03 18:48:54 -0600

    Ok, changing the markup was easy enough.

--- 2-seeourminds-prototype/Site/home/views.py
+++ 2-seeourminds-prototype/Site/home/views.py
@@ -16,15 +16,49 @@ class HomePageView(View):
 
     def dispatch(request, *args, **kwargs):
         response_text = textwrap.dedent('''\
-            <html>
-            <head>
-                <title>SeeOurMinds.com</title>
-            </head>
-            <body>
-                <h1>Welcome!</h1>
-                <p>Welcome to SeeOurMinds.com!</p>
-            </body>
-            </html>
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+
+        <div class="header-container">
+            <header class="wrapper clearfix">
+                <h1 class="title">h1.title</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">nav ul li a</a></li>
+                        <li><a href="#">nav ul li a</a></li>
+                        <li><a href="#">nav ul li a</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+
+                <article>
+                    <header>
+                        <h1>abfab h1</h1>
+                        <p>Say hi to the visitors, sweetie dahrlin!</p>
+                    </header>
+                </article>
+            </div><!-- .main .wrapper .clearfix -->
+        </div><!-- .main-container -->
+
+    </body>
+</html>
         ''')
         return HttpResponse(response_text)
 
