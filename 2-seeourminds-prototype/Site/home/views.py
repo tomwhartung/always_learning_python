01d898f95bc41c8d283b4bd219a5af271d7c4700
Tom W Hartung <tomwhartung@gmail.com>
2016-10-03 18:32:28 -0600

    Updated home/views.py as per the tutorial and what we did in 0-hello_world/.../say_hi_tomh .

--- 2-seeourminds-prototype/Site/home/views.py
+++ 2-seeourminds-prototype/Site/home/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+##
+## From the Writing the code section of
+##    http://dfpp.readthedocs.io/en/latest/chapter_01.html#writing-the-code
+##
+import textwrap
+
+from django.http import HttpResponse
+from django.views.generic.base import View
+
+
+class HomePageView(View):
+
+    def dispatch(request, *args, **kwargs):
+        response_text = textwrap.dedent('''\
+            <html>
+            <head>
+                <title>SeeOurMinds.com</title>
+            </head>
+            <body>
+                <h1>Welcome!</h1>
+                <p>Welcome to SeeOurMinds.com!</p>
+            </body>
+            </html>
+        ''')
+        return HttpResponse(response_text)
+
