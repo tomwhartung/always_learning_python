c2d21af300d4e962ce882d93f34c849b69aedb1e
Tom W Hartung <tomwhartung@gmail.com>
2016-10-11 16:19:02 -0600

    Hmm well I thought that having some markup would be preferable to just having "hello world" in bare text - and I was wrong.  Put that code in a new function named applicationMarkup for possible future reference.

--- 3-mod_wsgi/Site/sample_app.py
+++ 3-mod_wsgi/Site/sample_app.py
@@ -22,3 +22,23 @@ def application(environ, start_response):
 
    return [output]
 
+##
+#  Hmm well I thought that having some markup would be preferable to
+#  just having "hello world" in bare text - and I was wrong.  Rats!
+#
+def applicationMarkup(environ, start_response):
+   status = '200 OK'
+   markup = '<!doctype html><html>'
+   markup += '<head><meta charset="utf-8"></head>'
+   markup += '<body><main>'
+   markup += '<p>Hello world from Site/sample_app.py !</p>'
+   markup += '<p>Python version is: ' + sys.version + '</p>'
+   markup += '</main></body></html>'
+   output = markup.encode( 'utf-8' )
+
+   response_headers = [('Content-type', 'text/plain'),
+                       ('Content-Length', str(len(output)))]
+   start_response(status, response_headers)
+
+   return [output]
+
