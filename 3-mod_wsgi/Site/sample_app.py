cfc70d41ce62d24e92f4112d46f5825d79df931e
Tom W Hartung <tomwhartung@gmail.com>
2016-10-11 00:23:52 -0600

    Adding sample_app.py from https://modwsgi.readthedocs.io/en/develop/user-guides/quick-configuration-guide.html .

---
+++ 3-mod_wsgi/Site/sample_app.py
@@ -0,0 +1,17 @@
+#
+# Sample app from:
+#   https://modwsgi.readthedocs.io/en/develop/user-guides/quick-configuration-guide.html
+#
+# Using this to test our mod_wsgi install
+#
+
+def application(environ, start_response):
+    status = '200 OK'
+    output = 'Hello World!'
+
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+
+    return [output]
+
