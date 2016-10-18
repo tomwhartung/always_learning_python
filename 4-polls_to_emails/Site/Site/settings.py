3bca054c7649ea24dedabcaad52ee48748839d3f
Tom W Hartung <tomwhartung@gmail.com>
2016-10-17 20:17:49 -0600

    Added get_emails.apps.GetEmailsConfig to the list of INSTALLED_APPS in Site/settings.py .

--- 4-polls_to_emails/Site/Site/settings.py
+++ 4-polls_to_emails/Site/Site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'get_emails.apps.GetEmailsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
