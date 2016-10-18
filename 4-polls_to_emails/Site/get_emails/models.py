f1f354f7b495a44a22f98a4ed6a88d30e57bd871
Tom W Hartung <tomwhartung@gmail.com>
2016-10-18 11:16:56 -0600

    Added SITE_CODE_CHOICES , giving the site_code column a default value.  Updated the __str__ method to display the name and site_code along with the email.

--- 4-polls_to_emails/Site/get_emails/models.py
+++ 4-polls_to_emails/Site/get_emails/models.py
@@ -7,12 +7,28 @@ from django.db import models
 # SubscriberEmail model
 #
 class SubscriberEmail( models.Model ) :
+   GROJA_COM = 'gr'
+   SEEOURMINDS_COM = 'sm'
+   TOMWHARTUNG_COM = 'tw'
+   TOMHARTUNG_COM = 'th'
+   SITE_UNKNOWN = 'uk'
+   SITE_CODE_CHOICES = (
+      ( GROJA_COM, 'groja.com' ),
+      ( SEEOURMINDS_COM, 'seeourminds.com' ),
+      ( TOMWHARTUNG_COM, 'tomwhartung.com' ),
+      ( TOMHARTUNG_COM, 'tomhartung.com' ),
+      ( SITE_UNKNOWN, 'site_unknown' ),
+   )
    name = models.CharField( max_length=45 )
    email = models.CharField( max_length=254 )
-   site_code = models.CharField( max_length=2 )
+   site_code = models.CharField(
+      max_length=2,
+      choices=SITE_CODE_CHOICES,
+      default=SITE_UNKNOWN,
+   )
    subscription_date = models.DateTimeField('date subscribed')
 
    def __str__( self ) :
-      subscriberString = self.email + ' (' + self.name + ')' + self.site_code
-      return self.email
+      subscriberString = self.email + ' (' + self.name + ') ' + self.site_code
+      return subscriberString
 
