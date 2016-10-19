ea603c5f90d2da7b0cbb595a4b1fe4fa422285ef
Tom W Hartung <tomwhartung@gmail.com>
2016-10-19 09:36:14 -0600

    Added a nascent view for what should evolve into our subscription form.  Working through the relevant parts of part 3 of the tutorial on djangoproject.com .

--- 4-polls_to_emails/Site/get_emails/views.py
+++ 4-polls_to_emails/Site/get_emails/views.py
@@ -6,4 +6,11 @@ from django.http import HttpResponse
 # See https://docs.djangoproject.com/en/1.10/intro/tutorial01/#write-your-first-view
 #
 def index(request):
-    return HttpResponse( 'Hi from the get_emails index.' )
+   return HttpResponse( 'Hi from the get_emails index.' )
+
+##
+# Super-simple (Un)Subscribe form
+#
+def subscribe( request ) :
+   return HttpResponse( 'TODO: build the get_emails subscribe form.' )
+
