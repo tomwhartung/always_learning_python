8e522609a267dbf647f60d9e9e97fe4737571da2
Tom W Hartung <tomwhartung@gmail.com>
2016-10-19 13:52:55 -0600

    Commented out the old, "modelly way" of getting the counts.  Views should be dumb, so we moved that functionality - minimal as it is - to the model.  Ymmv.

--- 4-polls_to_emails/Site/get_emails/views.py
+++ 4-polls_to_emails/Site/get_emails/views.py
@@ -7,9 +7,18 @@ from .models import SubscriberEmail
 # See https://docs.djangoproject.com/en/1.10/intro/tutorial01/#write-your-first-view
 #
 def index(request):
-   subscribedCount = SubscriberEmail.objects.filter(subscribed=True).count()
-   unsubscribedCount = SubscriberEmail.objects.filter(subscribed=False).count()
-   totalCount = SubscriberEmail.objects.all().count()
+   ##
+   ## First try, but it could be considered "doing modelly things in the view:"
+   ##
+   ## subscribedCount = SubscriberEmail.objects.filter(subscribed=True).count()
+   ## unsubscribedCount = SubscriberEmail.objects.filter(subscribed=False).count()
+   ## totalCount = SubscriberEmail.objects.all().count()
+   #
+   # A better way, I think, "more pure:"
+   #
+   subscribedCount = SubscriberEmail.getCountSubscribed()
+   unsubscribedCount = SubscriberEmail.getCountUnsubscribed()
+   totalCount = SubscriberEmail.getCountAll()
    response = 'Hi from the get_emails index.  '
    response += 'Number of subscribed emails: ' + str( subscribedCount ) + '. '
    response += 'Number of unsubscribed emails: ' + str( unsubscribedCount ) + '. '
