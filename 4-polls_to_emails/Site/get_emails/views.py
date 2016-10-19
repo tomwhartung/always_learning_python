feae0830c2bdb35be29c610d6c50feedd8abc793
Tom W Hartung <tomwhartung@gmail.com>
2016-10-19 14:52:49 -0600

    Updated views.py to use the render function, as per the tutorial, and deleted a bunch of comments.

--- 4-polls_to_emails/Site/get_emails/views.py
+++ 4-polls_to_emails/Site/get_emails/views.py
@@ -8,26 +8,16 @@ from .models import SubscriberEmail
 # See https://docs.djangoproject.com/en/1.10/intro/tutorial01/#write-your-first-view
 #
 def index(request):
-   ####
-   #### First try, but it could be considered "doing modelly things in the view:"
-   ####
-   ## subscribedCount = SubscriberEmail.objects.filter(subscribed=True).count()
-   ## unsubscribedCount = SubscriberEmail.objects.filter(subscribed=False).count()
-   ## totalCount = SubscriberEmail.objects.all().count()
-   ##
-   ## Second try: a better way, I think, "more pure:"
-   ##
    subscribedCount = SubscriberEmail.getCountSubscribed()
    unsubscribedCount = SubscriberEmail.getCountUnsubscribed()
    totalCount = SubscriberEmail.getCountAll()
 
-   template = loader.get_template('get_emails/index.html')
    context = {
       'subscribedCount': subscribedCount,
       'unsubscribedCount': unsubscribedCount,
       'totalCount': totalCount,
    }
-   return HttpResponse( template.render(context,request) )
+   return render( request, 'get_emails/index.html', context )
 
 ##
 # Super-simple (Un)Subscribe form
