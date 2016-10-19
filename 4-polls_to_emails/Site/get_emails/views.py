78d3166f175b368e825b8033f763ee612830857b
Tom W Hartung <tomwhartung@gmail.com>
2016-10-19 13:36:19 -0600

    Added output of subscribed, unsubscribed, and total counts to index view in views.py .

--- 4-polls_to_emails/Site/get_emails/views.py
+++ 4-polls_to_emails/Site/get_emails/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import SubscriberEmail
 
 ##
 # See https://docs.djangoproject.com/en/1.10/intro/tutorial01/#write-your-first-view
 #
 def index(request):
-   return HttpResponse( 'Hi from the get_emails index.' )
+   subscribedCount = SubscriberEmail.objects.filter(subscribed=True).count()
+   unsubscribedCount = SubscriberEmail.objects.filter(subscribed=False).count()
+   totalCount = SubscriberEmail.objects.all().count()
+   response = 'Hi from the get_emails index.  '
+   response += 'Number of subscribed emails: ' + str( subscribedCount ) + '. '
+   response += 'Number of unsubscribed emails: ' + str( unsubscribedCount ) + '. '
+   response += 'Total of all emails: ' + str( totalCount ) + '. '
+   return HttpResponse( response )
 
 ##
 # Super-simple (Un)Subscribe form
