d2f1d878016aa5f9d2574edd8f8e1875ce188175
Tom W Hartung <tomwhartung@gmail.com>
2016-10-21 10:36:40 -0600

    Updated the index function to get and display the user agent, and added a few more comments to hello.py .

--- 5-flask_test_drive/Site/hello.py
+++ 5-flask_test_drive/Site/hello.py
@@ -1,16 +1,17 @@
 
 ##
 # From Chapter 2 of Flas Web Development
+# --------------------------------------
 #
 from flask import Flask
 app = Flask( __name__ )
 
-##
-# First, we say hello.
-#
-@app.route( '/' )
-def index() :
-   return '<h3>Hi there, earthling!</h3><p>Take me to your leader!</p>'
+## ##
+## # First, we say hello.
+## #
+## @app.route( '/' )
+## def index() :
+##    return '<h3>Hi there, earthling!</h3><p>Take me to your leader!</p>'
 
 ##
 # Second, we personalize the message
@@ -20,6 +21,18 @@ def index() :
 def user( name ) :
    return '<h3>Hi there, %s!</h3><p>Who\'s in charge around here?</p>' % name
 
+##
+# Third, we take a look at the user agent(!)
+#
+from flask import request
+@app.route( '/' )
+def index() :
+   user_agent = request.headers.get( 'User-Agent' )
+   return '<h3>Yo dude!</h3><p>I can see your browser is "%s" !</p>' % user_agent
+
+##
+# Run the app!
+#
 if __name__ == '__main__' :
    app.run( debug=True )
 
