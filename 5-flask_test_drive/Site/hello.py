d82a50c1dbea62eb6b19c66d7ec40619d60e7274
Tom W Hartung <tomwhartung@gmail.com>
2016-10-21 10:12:49 -0600

    Added a new function with a "dynamic route" and a few comments to hello.py .

--- 5-flask_test_drive/Site/hello.py
+++ 5-flask_test_drive/Site/hello.py
@@ -5,9 +5,20 @@
 from flask import Flask
 app = Flask( __name__ )
 
+##
+# First, we say hello.
+#
 @app.route( '/' )
 def index() :
    return '<h3>Hi there, earthling!</h3><p>Take me to your leader!</p>'
+
+##
+# Second, we personalize the message
+# This is called a "dynamic route"
+#
+@app.route( '/user/<name>' )
+def user( name ) :
+   return '<h3>Hi there, %s!</h3><p>Who\'s in charge around here?</p>' % name
 
 if __name__ == '__main__' :
    app.run( debug=True )
