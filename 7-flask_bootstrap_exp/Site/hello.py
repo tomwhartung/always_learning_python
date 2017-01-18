2e34330a0cfdcd03495a7092ed817a0f9a07bdf0
Tom W Hartung <tomwhartung@gmail.com>
2017-01-18 10:36:31 -0700

    Updated 7-flask_bootstrap_exp/Site/hello.py to use the new template, base.html .

--- 7-flask_bootstrap_exp/Site/hello.py
+++ 7-flask_bootstrap_exp/Site/hello.py
@@ -12,7 +12,7 @@ app = Flask( __name__ )
 #
 @app.route( '/' )
 def index() :
-   return render_template( 'index.html' )
+   return render_template( 'base.html' )
 
 ##
 # Allow for personalizing the message
