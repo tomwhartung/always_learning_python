fe8c178bbdeb8372d68c526980e5f0aca3cc9b64
Tom W Hartung <tomwhartung@gmail.com>
2017-01-24 15:59:58 -0700

    Updated to set the appropriate "*Selected" variable when rendering a template.

--- 6-flask_templates_exp/Site/groja.py
+++ 6-flask_templates_exp/Site/groja.py
@@ -13,28 +13,28 @@ app = Flask( __name__ )
 #
 @app.route( '/' )
 def home() :
-   return render_template( 'home.html' )
+   return render_template( 'home.html', homeSelected='selected' )
 
 ##
 # Show the About page:
 #
 @app.route( '/about' )
 def about() :
-   return render_template( 'about.html' )
+   return render_template( 'about.html', aboutSelected='selected' )
 
 ##
 # Show the Books and Sites page:
 #
 @app.route( '/booksandsites' )
 def booksandsites() :
-   return render_template( 'booksandsites.html' )
+   return render_template( 'booksandsites.html', booksandsitesSelected='selected' )
 
 ##
 # Show the Your Portrait page:
 #
 @app.route( '/yourportrait' )
 def yourportrait() :
-   return render_template( 'yourportrait.html' )
+   return render_template( 'yourportrait.html', yourportraitSelected='selected' )
 
 ##
 # Run the app!
