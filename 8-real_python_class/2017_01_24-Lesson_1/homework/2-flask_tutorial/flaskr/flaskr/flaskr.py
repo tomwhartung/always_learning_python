321dd42a5afd31bfcb34fc12adb71e43e5565847
Tom W Hartung <tomwhartung@gmail.com>
2017-02-06 15:17:38 -0700

    Added the view functions.

--- 8-real_python_class/2017_01_24-Lesson_1/homework/2-flask_tutorial/flaskr/flaskr/flaskr.py
+++ 8-real_python_class/2017_01_24-Lesson_1/homework/2-flask_tutorial/flaskr/flaskr/flaskr.py
@@ -9,6 +9,10 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
+################################################################################
+#
+# Mainline code to create the app and load the config, allowing for overrides
+# ---------------------------------------------------------------------------
 #
 # Create the application instance :)
 # From Step 2 in the tutorial
@@ -28,7 +32,72 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+################################################################################
 #
+# View functions
+# --------------
+#
+##
+# Index (Home page) view to show entries
+# From Step 6 in the tutorial
+#
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+##
+# Page to allow adding a new entry
+# Form appears on show entries page when user is logged in
+# From Step 6 in the tutorial
+#
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+##
+# Login page
+# From Step 6 in the tutorial
+#
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+##
+# Logout page
+# From Step 6 in the tutorial
+#
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+################################################################################
+#
+# Functions for working with the Database
+# ---------------------------------------
+#
+##
 # Connect to the specific database.
 # From Step 2 in the tutorial
 #
@@ -37,7 +106,7 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
-#
+##
 # Function to initialize the database.
 # From Step 5 in the tutorial
 #
@@ -47,7 +116,7 @@ def init_db():
         db.cursor().executescript(f.read())
     db.commit()
 
-#
+##
 # Command-line command to initialize the database.
 # From Step 5 in the tutorial
 #
@@ -56,7 +125,7 @@ def initdb_command():
     init_db()
     print('Initialized the database.')
 
-#
+##
 # Opens a new database connection if there is none yet for the current application context.
 # From Step 4 in the tutorial
 #
@@ -65,7 +134,7 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
-#
+##
 # Closes the database again at the end of the request.
 # From Step 4 in the tutorial
 #
