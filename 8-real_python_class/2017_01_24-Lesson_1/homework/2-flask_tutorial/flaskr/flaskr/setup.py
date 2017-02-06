f7774c36845016eb36e63380ebcd7c3e420d35fc
Tom W Hartung <tomwhartung@gmail.com>
2017-02-06 12:55:37 -0700

    What we need for the setup.py file, or so they tell me.

---
+++ 8-real_python_class/2017_01_24-Lesson_1/homework/2-flask_tutorial/flaskr/flaskr/setup.py
@@ -0,0 +1,13 @@
+##
+# Setup file for flaskr app
+#
+from setuptools import setup
+
+setup(
+    name='flaskr',
+    packages=['flaskr'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
