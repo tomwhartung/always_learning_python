574ddd037661aae5c8fad6db4b5b59d30e3ad7d4
Tom W Hartung <tomwhartung@gmail.com>
2017-02-18 11:02:25 -0700

    Updated forms.py with validators.

--- 11-flask_wtforms_exp-2/Site/app/forms.py
+++ 11-flask_wtforms_exp-2/Site/app/forms.py
@@ -2,12 +2,17 @@
 # Reference:
 #   From the first section, "Creating a Form:"
 #     https://code.tutsplus.com/tutorials/intro-to-flask-adding-a-contact-page--net-28982
+#   Added updates from the second part of the tutorial:
+#     https://code.tutsplus.com/tutorials/intro-to-flask-adding-a-contact-page--net-28982
 #
-from wtforms import Form, TextField, TextAreaField, SubmitField
- 
+from wtforms import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
+
 class ContactForm(Form):
-  name = TextField("Name")
-  email = TextField("Email")
-  subject = TextField("Subject")
-  message = TextAreaField("Message")
-  submit = SubmitField("Send")
+  ## name = TextField( "Name",  [validators.Required("Please enter your name.")] )
+  name = TextField( "Name" )
+  email = TextField( "Email",
+    [ validators.Required("Please enter your email address."),
+      validators.Email("Please enter your email address") ] )
+  subject = TextField("Subject",  [validators.Required("Please enter a subject line.")])
+  message = TextAreaField( "Message",  [validators.Required("Please enter a message.")] )
+  submit = SubmitField( "Send" )
