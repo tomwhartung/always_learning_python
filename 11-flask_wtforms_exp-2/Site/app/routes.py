9304228f4aef415213bc516b07269ae0a09d70b4
Tom W Hartung <tomwhartung@gmail.com>
2017-02-18 11:56:18 -0700

    Updated routes.py to flash the error messages.

--- 11-flask_wtforms_exp-2/Site/app/routes.py
+++ 11-flask_wtforms_exp-2/Site/app/routes.py
@@ -6,8 +6,8 @@
 #
 from flask import Flask, render_template, request, flash
 from forms import ContactForm
- 
-app = Flask(__name__)      
+
+app = Flask(__name__)
 #
 # Update the app's configuration for CSRF prevention
 #
@@ -16,7 +16,7 @@ app.secret_key = 'development key'
 @app.route('/')
 def home():
   return render_template('home.html')
- 
+
 @app.route('/about')
 def about():
   return render_template('about.html')
@@ -26,7 +26,11 @@ def contact():
   form = ContactForm()
 
   if request.method == 'POST':
-    return 'Form posted.'
+    if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('contact.html', form=form)
+    else:
+      return 'Form posted.'
   elif request.method == 'GET':
     return render_template('contact.html', form=form)
 
