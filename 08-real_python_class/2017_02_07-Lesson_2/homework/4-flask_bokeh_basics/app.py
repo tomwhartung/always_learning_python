881f6993b533c51d1f47e0719cdaeac4f8d2e1c8
Tom W Hartung <tomwhartung@gmail.com>
2017-02-20 12:34:55 -0700

    Phase 1 of 4-flask_bokeh_basics is a hello world program.

---
+++ 08-real_python_class/2017_02_07-Lesson_2/homework/4-flask_bokeh_basics/app.py
@@ -0,0 +1,17 @@
+##
+#  From the real python flask bokeh basics:
+#    
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ 08-real_python_class/2017_02_07-Lesson_2/homework/4-flask_bokeh_basics/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Run the flask development server
+#
+export FLASK_APP=app.py
+python3 -m flask run
