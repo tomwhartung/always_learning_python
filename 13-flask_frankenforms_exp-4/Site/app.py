f4e31083c9491dae1c6255c12e3f9eba43c6d7bc
Tom W Hartung <tomwhartung@gmail.com>
2017-02-20 14:39:40 -0700

    Removed code for the password from app.py .

--- 13-flask_frankenforms_exp-4/Site/app.py
+++ 13-flask_frankenforms_exp-4/Site/app.py
@@ -15,12 +15,10 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
     email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
-    password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
 
     def reset(self):
         blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
         self.process(blankData)
-
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -30,9 +28,8 @@ def hello():
     print form.errors
     if request.method == 'POST':
         name=request.form['name']
-        password=request.form['password']
         email=request.form['email']
-        print name, " ", email, " ", password
+        print name, " ", email
 
         if form.validate():
             # Save the comment here.
@@ -44,4 +41,3 @@ def hello():
 
 if __name__ == "__main__":
     app.run()
-
