2207ee6b3171d34a83ce45ef11ba20861d095720
Tom W Hartung <tomwhartung@gmail.com>
2017-02-20 10:25:48 -0700

    Renamed rp-2-python3_6_flask_bokeh* to rp-2-python3_6_flask_bokeh_bette* .

--- 08-real_python_class/2017_02_07-Lesson_2/homework/3-bokeh_quickstart/env.sh
+++ 08-real_python_class/2017_02_07-Lesson_2/homework/3-bokeh_quickstart/env.sh
@@ -1,1 +1,1 @@
-. /var/www/always_learning/github/customizations/always_learning_python/virtualenvs/rp-2-python3_6_flask_bokeh.sh
+. /var/www/always_learning/github/customizations/always_learning_python/virtualenvs/rp-2-python3_6_flask_bokeh_bette.sh
--- virtualenvs/rp-2-python3_6_flask_bokeh.sh
+++
@@ -1,1 +0,0 @@
-. /var/www/always_learning/github/customizations/always_learning_python/virtualenvs/rp-2-python3_6_flask_bokeh/bin/activate
