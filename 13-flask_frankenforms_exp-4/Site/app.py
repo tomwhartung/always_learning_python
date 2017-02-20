4361f1eb19a077968561a7a7b2c951473b48fd8f
Tom W Hartung <tomwhartung@gmail.com>
2017-02-20 15:33:12 -0700

    Renamed ReusableForm to NameEmailForm and changed all print statements to use the print() function.  Also adjusted indentation to 3 spaces, because 4 is too many and 2 is too few.

--- 13-flask_frankenforms_exp-4/Site/app.py
+++ 13-flask_frankenforms_exp-4/Site/app.py
@@ -1,43 +1,42 @@
 ##
-# Create a form to get the visitor's name
-# Reference:
-#   https://pythonspot.com/flask-web-forms/
+#  Create a form to get the visitor's name and email address
+#  Reference:
+#     https://pythonspot.com/flask-web-forms/
 #
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
-# App config.
+#  App config.
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-    email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
+class NameEmailForm( Form ):
+   name = TextField( 'Name:', validators=[validators.required()] )
+   email = TextField( 'Email:', validators=[validators.required(), validators.Length(min=6, max=35)] )
 
-    def reset(self):
-        blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
-        self.process(blankData)
+   def reset(self):
+      blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
+      self.process(blankData)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    form = ReusableForm(request.form)
+   form = NameEmailForm( request.form )
 
-    print form.errors
-    if request.method == 'POST':
-        name=request.form['name']
-        email=request.form['email']
-        print name, " ", email
+   print( "form.errors:", form.errors )
+   if request.method == 'POST':
+      name=request.form['name']
+      email=request.form['email']
+      print( "name", name, "email", email )
 
-        if form.validate():
-            # Save the comment here.
-            flash('Thanks for registration ' + name)
-        else:
-            flash('Error: All the form fields are required. ')
+      if form.validate():
+         flash( 'Thanks, we will be in touch with you soon, ' + name + '!' )
+      else:
+         flash( 'Error: All the form fields are required.' )
 
-    return render_template('hello.html', form=form)
+   return render_template( 'hello.html', form=form )
 
 if __name__ == "__main__":
     app.run()
