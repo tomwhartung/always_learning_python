bb4808e4ff6ee924e3cf81ec0eb9dc0629cbe7aa
Tom W Hartung <tomwhartung@gmail.com>
2017-02-21 11:31:03 -0700

    Moved the code for the form out of app.py and into form.py , so that we can be better oraniz-ized (sic).

--- 13-flask_frankenforms_exp-4/Site/app.py
+++ 13-flask_frankenforms_exp-4/Site/app.py
@@ -4,26 +4,13 @@
 #     https://pythonspot.com/flask-web-forms/
 #
 from flask import Flask, render_template, flash, request
-from wtforms import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Optional, Required, Email
+from form import NameEmailForm
 
 #  App config.
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
-
-class NameEmailForm( Form ):
-   name = StringField( 'Name:', validators=[Optional()] )
-   email = StringField( 'Email:',
-      [Required("Please share your email address so that we can contact you."),
-       Email("Please enter a valid email address.")] )
-   submit = SubmitField( 'Get Your Portrait' )
-
-   def reset(self):
-      blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
-      self.process(blankData)
 
 
 @app.route("/", methods=['GET', 'POST'])
