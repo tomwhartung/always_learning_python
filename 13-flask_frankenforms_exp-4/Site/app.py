a8075626abf92f45d52a20ecd52049af502ad1b2
Tom W Hartung <tomwhartung@gmail.com>
2017-02-21 11:21:20 -0700

    Made a few almost rather cosmetic changes, and more importantly, fixed it so it displays the correct error message(s).

--- 13-flask_frankenforms_exp-4/Site/app.py
+++ 13-flask_frankenforms_exp-4/Site/app.py
@@ -6,7 +6,7 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required, Email
+from wtforms.validators import Optional, Required, Email
 
 #  App config.
 DEBUG = True
@@ -15,7 +15,7 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 class NameEmailForm( Form ):
-   name = StringField( 'Name:' )
+   name = StringField( 'Name:', validators=[Optional()] )
    email = StringField( 'Email:',
       [Required("Please share your email address so that we can contact you."),
        Email("Please enter a valid email address.")] )
@@ -30,16 +30,18 @@ class NameEmailForm( Form ):
 def hello():
    form = NameEmailForm( request.form )
 
-   print( "form.errors:", form.errors )
    if request.method == 'POST':
-      name = request.form['name']
-      email = request.form['email']
+      name = form.name.data
+      email = form.email.data
       print( "name: ", name, "email: ", email )
 
       if form.validate():
          flash( 'Thanks, we will be in touch with you soon, ' + name + '!' )
       else:
-         flash( 'Error: All the form fields are required.' )
+         print( "form.errors:", form.errors )
+         ## for error_field in form.errors:
+         ##    flash( error_field.key, ":", error_field.value )
+         flash( form.errors )
 
    return render_template( 'hello.html', form=form )
 
