07ab0d8fa81e3f99615edc94c31d9c49396d91d7
Tom W Hartung <tomwhartung@gmail.com>
2017-02-23 12:33:58 -0700

    Updated NameEmailSchema.sql with the correct comment characters.

--- 14-flask_frankenform_with_db/db/NameEmailSchema.sql
+++ 14-flask_frankenform_with_db/db/NameEmailSchema.sql
@@ -1,9 +1,11 @@
-#
-#  Schema for our NameEmail table
-#  ------------------------------
-#  Reference for datatypes:
-#     https://www.sqlite.org/datatype3.html
-#
+--
+--  Schema for our NameEmail table
+--  ------------------------------
+--  Reference for datatypes:
+--     https://www.sqlite.org/datatype3.html
+--  Note the date_* columns are stored as Unix Time in integers, specifically:
+--     "INTEGER as Unix Time, the number of seconds since 1970-01-01 00:00:00 UTC"
+--
 CREATE TABLE NameEmail
    ( name TEXT,
      email TEXT,
