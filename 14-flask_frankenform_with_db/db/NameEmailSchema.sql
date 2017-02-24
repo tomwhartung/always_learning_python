6acd4e793ca83ffb3752992f74ff970b38918a55
Tom W Hartung <tomwhartung@gmail.com>
2017-02-23 18:58:36 -0700

    Updated NameEmailSchema.sql to set the date_* variables to the CURRENT_TIMESTAMP .  Note that this changes the format of the data in these columns.

--- 14-flask_frankenform_with_db/db/NameEmailSchema.sql
+++ 14-flask_frankenform_with_db/db/NameEmailSchema.sql
@@ -3,8 +3,8 @@
 --  ------------------------------
 --  Reference for datatypes:
 --     https://www.sqlite.org/datatype3.html
---  Note the date_* columns are stored as Unix Time in integers, specifically:
---     "INTEGER as Unix Time, the number of seconds since 1970-01-01 00:00:00 UTC"
+--  Note the date_* columns are stored as strings, specifically:
+--     CURRENT_TIMESTAMP returns dates in the format "1970-01-01 00:00:00"
 --
 CREATE TABLE NameEmail
    ( id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,8 +12,8 @@ CREATE TABLE NameEmail
      email TEXT,
      site TEXT DEFAULT 'groja.com',
      active INTEGER DEFAULT 1,
-     date_added INTEGER,
-     date_changed INTEGER,
+     date_added TEXT DEFAULT CURRENT_TIMESTAMP,
+     date_changed TEXT DEFAULT CURRENT_TIMESTAMP,
      consulting INTEGER DEFAULT 0,
      newsletter INTEGER DEFAULT 0,
      portrait INTEGER DEFAULT 0
