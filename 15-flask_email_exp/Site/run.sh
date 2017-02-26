fc32bf9a01545e8cf350bf0ba805ae951c61a5ca
Tom W Hartung <tomwhartung@gmail.com>
2017-02-26 11:56:23 -0700

    Tried to update run.sh to be more like the one we are using on jane for groja.com , and got a strange warning.  Added comments to run.sh so that we don't waste time trying this again.

--- 15-flask_email_exp/Site/run.sh
+++ 15-flask_email_exp/Site/run.sh
@@ -3,4 +3,14 @@
 # Run the flask development server
 #
 export FLASK_DEBUG=1
+##
+## ** Trying to run the app using this technique (used on groja.com) causes a warning on bette:
+##    "/usr/lib/python3.6/runpy.py:125: RuntimeWarning: 'flask.cli' found in sys.modules
+##      after import of package 'flask', but prior to execution of 'flask.cli';
+##       this may result in unpredictable behaviour"
+## export FLASK_APP=app.py
+## python -m flask run
+## python3 -m flask run
+## ** Seriously wtf! **
+##
 python app.py
