b2ac4db95e588b9ae625e89483d54035c2cb6a52
Tom W Hartung <tomwhartung@gmail.com>
2017-02-26 11:43:27 -0700

    Removed line setting the DEBUG flag from app.py .

--- 15-flask_email_exp/Site/app.py
+++ 15-flask_email_exp/Site/app.py
@@ -9,7 +9,6 @@ from form import NameEmailForm
 from db_access import insert_name_email
 
 #  App config.
-DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
