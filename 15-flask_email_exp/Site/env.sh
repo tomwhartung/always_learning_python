808839f2d93e712ff33954960a1c6c1c12ebbc70
Tom W Hartung <tomwhartung@gmail.com>
2017-02-25 18:38:07 -0700

    Updated env.sh as appropriate for the 15-flask_email_exp project.

--- 15-flask_email_exp/Site/env.sh
+++ 15-flask_email_exp/Site/env.sh
@@ -1,1 +1,1 @@
-. /var/www/always_learning/github/customizations/always_learning_python/virtualenvs/p3_flask_bootstrap_wtf_bette.sh
+. /var/www/always_learning/github/customizations/always_learning_python/virtualenvs/p3_flask_mail_etc.sh
