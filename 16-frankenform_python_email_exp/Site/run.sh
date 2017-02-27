94234228cd0cab9901f2cdcc1a4655f1e500c75f
Tom W Hartung <tomwhartung@gmail.com>
2017-02-27 11:41:07 -0700

    Updated run.sh to set the FLASK_DEBUG flag, and to run groja.py (instead of app.py).  Also added a few comments concerning how we'd like things to be ....

--- 16-frankenform_python_email_exp/Site/run.sh
+++ 16-frankenform_python_email_exp/Site/run.sh
@@ -2,4 +2,12 @@
 #
 # Run the flask development server
 #
-python app.py
+## #
+## #  We'd like to be able to run it like we run groja.com ...
+## #
+## export FLASK_APP=groja.py
+## python -m flask run
+## python3 -m flask run
+##
+export FLASK_DEBUG=1
+python groja.py
