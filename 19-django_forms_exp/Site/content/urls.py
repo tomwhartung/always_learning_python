ff7a0c279e5eea580890058850c86786d6769882
Tom W Hartung <tomwhartung@gmail.com>
2017-03-14 13:54:32 -0600

    Fixed urls.py so that we can see the form.

--- 19-django_forms_exp/Site/content/urls.py
+++ 19-django_forms_exp/Site/content/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
     #
     url(r'^quiz/name_form$', views.quiz_name_form, name='quiz_name_form'),
     url(r'^quiz/contact_form$', views.quiz_contact_form, name='quiz_contact_form'),
-    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+    ## url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+    url(r'^book$', views.renew_book_librarian, name='renew-book-librarian'),
 ]
