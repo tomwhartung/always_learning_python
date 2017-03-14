3505541ada6975f9c02b2e1cdd05948d2b63c274
Tom W Hartung <tomwhartung@gmail.com>
2017-03-14 14:30:38 -0600

    Backing out of experimental changes made to urls.py .

--- 20-som_quiz_form/Site/content/urls.py
+++ 20-som_quiz_form/Site/content/urls.py
@@ -13,11 +13,4 @@ urlpatterns = [
     url(r'^quiz$', views.quiz, name='quiz'),
     url(r'^google428ef5aab2bc0870.html$',
       views.google_verification, name='google_verification'),
-    #
-    # Urls for experimenting with forms
-    #
-    url(r'^quiz/name_form$', views.quiz_name_form, name='quiz_name_form'),
-    url(r'^quiz/contact_form$', views.quiz_contact_form, name='quiz_contact_form'),
-    ## url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
-    url(r'^book$', views.renew_book_librarian, name='renew-book-librarian'),
 ]
