0e666230e41032b16e43b7ed23d455e710455264
Tom W Hartung <tomwhartung@gmail.com>
2017-03-14 13:37:32 -0600

    Added renew_book_librarian from the forms example at developer.mozilla.org to views.py .

--- 19-django_forms_exp/Site/content/views.py
+++ 19-django_forms_exp/Site/content/views.py
@@ -130,3 +130,34 @@ def quiz_contact_form( request ):
       contact_form = ContactForm()
 
    return render(request, 'content/quiz.html', {'contact_form': contact_form})
+
+##
+#  RenewBookForm
+#  Reference:
+#     https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Forms
+#
+from .forms import RenewBookForm
+def renew_book_librarian(request, pk):
+   book_inst=get_object_or_404(BookInstance, pk = pk)
+
+   # If this is a POST request then process the Form data
+   if request.method == 'POST':
+
+      form = RenewBookForm(request.POST)    # Create a form instance and populate it with data from the request (binding):
+
+      if form.is_valid():
+         ## # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+         ## book_inst.due_back = form.cleaned_data['renewal_date']
+         ## book_inst.save()
+         print( "form.cleaned_data['renewal_date']", form.cleaned_data['renewal_date'] )
+
+         ## # redirect to a new URL:
+         ## return HttpResponseRedirect(reverse('all-borrowed') )
+         return HttpResponseRedirect('/quiz/')
+
+   #  If this is a GET (or any other method) create the default form.
+   else:
+      proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+      form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+
+   return render(request, 'content/book_renew_librarian.html', {'form': form})
