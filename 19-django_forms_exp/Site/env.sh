0e500f11344fbeb88724a85cf60a0f6005ccd9ab
Tom W Hartung <tomwhartung@gmail.com>
2017-03-17 11:24:22 -0600

    Adding an env.sh script containing only comments, because this matches what we are running on the sites, so we are running the versions that are installed globally.

---
+++ 19-django_forms_exp/Site/env.sh
@@ -0,0 +1,9 @@
+#
+#  This env.sh file is empty!
+#  --------------------------
+#  Live sites run using the global version of installed packages.
+#  The code for this project runs using these global versions.
+#
+#  NOTE:
+#     When developing, run python3 and pip3 instead of python and pip!
+#
