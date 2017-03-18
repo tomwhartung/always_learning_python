d51f0313d89e99aadaaf551652e2514ba201ae4d
Tom W Hartung <tomwhartung@gmail.com>
2017-03-18 12:50:57 -0600

    Our first choice is now showing up on the page - making progress in a baby steps sort of way!

--- 20-som_quiz_form/Site/content/forms.py
+++ 20-som_quiz_form/Site/content/forms.py
@@ -10,7 +10,7 @@ import os
 ##
 #  QuizForm: convert our json to a list of questions and multiple-choice answers
 #
-class QuizForm(forms.Form):
+class QuizForm( forms.Form ):
    quiz_dictionary = []
 
    ##
@@ -26,18 +26,25 @@ class QuizForm(forms.Form):
       quiz_dictionary = json.loads( quiz_json_string )
       return( quiz_dictionary )
 
-   def get_choices( question_no ):
-      ## print( 'self.quiz_dictionary:', self.quiz_dictionary )
-      print( 'question_no:', question_no )
-      choices = [[ '1', 'get_choices: first choice'], ['2', 'get_choices: second choice' ]]
+   ##
+   #  Get and return the answers that are populated in the json for the given question
+   #
+   def get_choices( quiz_question ):
+      print( 'get_choices - quiz_question:', quiz_question )
+      choices = []
+      if len(quiz_question['answer_1_text']) > 0 and int(quiz_question['answer_1_weight']) > 0 :
+         choice_1 = [ '1', quiz_question['answer_1_text'] ]
+         choices.append( choice_1 )
+
+      ## choices = [[ '1', 'get_choices: first choice'], ['2', 'get_choices: second choice' ]]
       return choices
 
    name = forms.CharField( max_length=50 )
    email = forms.EmailField()
-   choices_1 = get_choices( 1 )
-   ## choices_1 = [[ '1', 'First choice'], ['2', 'Second choice']]
-   question_1 = forms.ChoiceField( widget=forms.RadioSelect, choices=choices_1 )
    quiz_dictionary = read_quiz_json()
    ## print( 'quiz_dictionary:', quiz_dictionary )
    print( 'len(quiz_dictionary):', len(quiz_dictionary) )
-
+   print( 'quiz_dictionary[0]:', quiz_dictionary[0] )
+   ## choices_1 = [[ '1', 'First hard-coded choice'], ['2', 'Second hard-coded choice']]
+   choices_1 = get_choices( quiz_dictionary[0] )
+   question_1 = forms.ChoiceField( widget=forms.RadioSelect, choices=choices_1 )
