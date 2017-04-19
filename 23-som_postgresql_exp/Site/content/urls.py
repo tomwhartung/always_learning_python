5e379f4d9592e26b6cd3e901e603940c57ff9825
Tom W Hartung <tomwhartung@gmail.com>
2017-04-19 11:29:18 -0600

    Changed size to quiz_size , before that becomes any more difficult to do.

--- 23-som_postgresql_exp/Site/content/urls.py
+++ 23-som_postgresql_exp/Site/content/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^image/(?P<image_id>\w+)/$', views.image, name='image'),
     url(r'^quiz$', views.quiz, name='quiz'),
     url(r'^quiz/results$', views.quiz_results, name='quiz_results'),
-    url(r'^quiz/(?P<size>\w+)/$', views.quiz, name='quiz'),
+    url(r'^quiz/(?P<quiz_size>\w+)/$', views.quiz, name='quiz'),
     url(r'^google428ef5aab2bc0870.html$',
         views.google_verification, name='google_verification'),
 ]
