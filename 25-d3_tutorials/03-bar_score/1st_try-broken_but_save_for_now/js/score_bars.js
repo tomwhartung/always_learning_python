eb8560158164761f9fabb80a8b53022a75bb693f
Tom W Hartung <tomwhartung@gmail.com>
2017-05-11 11:15:51 -0600

    Saving a copy of the current version of code in new directory 1st_try-broken_but_save_for_now , while we try to fix whatever it was we messed up yesterday.

---
+++ 25-d3_tutorials/03-bar_score/1st_try-broken_but_save_for_now/js/score_bars.js
@@ -0,0 +1,20 @@
+/**
+ * score_bars.js: trying to use score json to create bars as described
+ *   in the Goal section of the README.md
+ * Starting with code from:
+ *   01-aligned_left/16-axes (16: Axes)
+ * Adding code and ideas from:
+ *   02-bullet_charts/04-removed_ranges
+ */
+var splot_dataset = [
+    [ 5,     20 ],
+    [ 460,  190 ],
+    [ 250,  150 ],
+    [ 100,  233 ],
+    [ 330,  495 ],
+    [ 410,  312 ],
+    [ 425,  244 ],
+    [ 25,   467 ],
+    [ 85,   121 ],
+    [ 220,   88 ]
+];
