9ee4de75f39d020bb17fc53407ceb1154f3c1bae
Tom W Hartung <tomwhartung@gmail.com>
2017-05-13 16:55:57 -0600

    Made many fairly minor changes, including adjusting indentation of almost all lines.  Removed function scoreBulletWidth and changed most mixedCase/camelCase variable and function names to use underscores, because that is how we are writing the python code (per PEP8 - omfg it's leaking over into my js code) so take that!

--- 25-d3_tutorials/03-bar_score/2nd-clean_it_up/js/score_bars.js
+++ 25-d3_tutorials/03-bar_score/2nd-clean_it_up/js/score_bars.js
@@ -23,116 +23,113 @@
 //     http://projects.instantcognition.com/protovis/bulletchart/
 //
 (function() {
-d3.score_bars = function() {
-  var orient = "left";
-  var reverse = false;
-  var get_score_pct_fcn = get_score_pct;
-  var width = 380;
-  var height = 30;
-  var tickFormat = null;
+  d3.score_bars = function() {
+    var orient = "left";
+    var reverse = false;
+    var get_score_pct_fcn = get_score_pct;
+    var width = 180;   // default value; to override, call width()
+    var height = 15;   // default value; to override, call height()
+    var tick_format = null;
+    /**
+     * Function to process each of the SVGGElements in the list
+     */
+    function score_bars(svgg_elt_list) {
+      console.log('score_bars - passed-in "svgg_elt_list" = ' + svgg_elt_list)
+      svgg_elt_list.each(function(data, index) {
+        var score_value = get_score_pct_fcn.call(this, data, index);
+        var this_svgg_elt = d3.select(this);
+        var score_value_arr = []
+        score_value_arr = []              // data() fcn expects an array
+        score_value_arr.push(score_value);
 
-  // For each small multiple…
-  function score_bars(svgg_elt_list) {
-    console.log('score_bars - passed-in "svgg_elt_list" = ' + svgg_elt_list)
-    svgg_elt_list.each(function(data, index) {
-      var score_value = get_score_pct_fcn.call(this, data, index);
-      var this_svgg_elt = d3.select(this);
-      console.log('score_bars - var "this_svgg_elt" = ' + this_svgg_elt)
-		var score_value_arr = []
-		score_value_arr = []
-		score_value_arr.push(score_value);
+        console.log('check check check 123 123 123 ccc 123 456 score_value: ' + score_value);
+        console.log('score_bars - width x height: ' + width + ' x ' + height)
 
-      console.log('check check check 123 123 123 bbb ccc 123 456 score_value: ' + score_value);
+        var x_scale = d3.scale.linear()
+            .domain([0, 100])
+            .range(reverse ? [width, 0] : [0, width]);
 
-      // Compute the new x-scale.
-      var x_scale = d3.scale.linear()
-          .domain([0, 100])
-          .range(reverse ? [width, 0] : [0, width]);
+        var score_pct_lines = this_svgg_elt.selectAll("line.score-pct")
+            .data(score_value_arr);
 
-      // Update the score-pct lines.
-      var score_pct_lines = this_svgg_elt.selectAll("line.score-pct")
-          .data(score_value_arr);
+        score_pct_lines.enter().append("line")
+            .attr("class", "score-pct")
+            .attr("x1", x_scale)
+            .attr("x2", x_scale)
+            .attr("y1", height / 6)
+            .attr("y2", height * 5 / 6);
 
-      score_pct_lines.enter().append("line")
-          .attr("class", "score-pct")
-          .attr("x1", x_scale)
-          .attr("x2", x_scale)
-          .attr("y1", height / 6)
-          .attr("y2", height * 5 / 6);
+        // Compute the tick format.
+        var format = tick_format || x_scale.tickFormat(8);
 
-      // Compute the tick format.
-      var format = tickFormat || x_scale.tickFormat(8);
+        // Update the tick groups.
+        var tick = this_svgg_elt.selectAll("g.tick")
+            .data(x_scale.ticks(8), function(data) {
+              return this.textContent || format(data);
+            });
 
-      // Update the tick groups.
-      var tick = this_svgg_elt.selectAll("g.tick")
-          .data(x_scale.ticks(8), function(data) {
-            return this.textContent || format(data);
-          });
+        // Add the ticks
+        var tick_enter = tick.enter().append("g")
+            .attr("class", "tick")
+            .attr("transform", score_bullet_translate(x_scale))
+            .style("opacity", 1);
 
-      // Add the ticks
-      var tickEnter = tick.enter().append("g")
-          .attr("class", "tick")
-          .attr("transform", scoreBulletTranslate(x_scale))
-          .style("opacity", 1);
+        tick_enter.append("line")
+            .attr("y1", height)
+            .attr("y2", height * 7 / 6);
 
-      tickEnter.append("line")
-          .attr("y1", height)
-          .attr("y2", height * 7 / 6);
+        tick_enter.append("text")
+            .attr("text-anchor", "middle")
+            .attr("dy", "1em")
+            .attr("y", height * 7 / 6)
+            .text(format);
 
-      tickEnter.append("text")
-          .attr("text-anchor", "middle")
-          .attr("dy", "1em")
-          .attr("y", height * 7 / 6)
-          .text(format);
+      });
+      d3.timer.flush();
+    }
 
-    });
-    d3.timer.flush();
+    // left, right, top, bottom
+    score_bars.orient = function(x) {
+      if (!arguments.length) return orient;
+      orient = x;
+      reverse = orient == "right" || orient == "bottom";
+      return score_bars;
+    };
+
+    /**
+     * Override default width
+     */
+    score_bars.width = function(x) {
+      if (!arguments.length) return width;
+      width = x;
+      return score_bars;
+    };
+
+    /**
+     * Override default height
+     */
+    score_bars.height = function(x) {
+      if (!arguments.length) return height;
+      height = x;
+      return score_bars;
+    };
+
+    score_bars.tick_format = function(x) {
+      if (!arguments.length) return tick_format;
+      tick_format = x;
+      return score_bars;
+    };
+
+    return score_bars;
+  };
+
+  function get_score_pct(data, index) {
+    return data.score_pct;
   }
 
-  // left, right, top, bottom
-  score_bars.orient = function(x) {
-    if (!arguments.length) return orient;
-    orient = x;
-    reverse = orient == "right" || orient == "bottom";
-    return score_bars;
-  };
-
-  score_bars.width = function(x) {
-    if (!arguments.length) return width;
-    width = x;
-    return score_bars;
-  };
-
-  score_bars.height = function(x) {
-    if (!arguments.length) return height;
-    height = x;
-    return score_bars;
-  };
-
-  score_bars.tickFormat = function(x) {
-    if (!arguments.length) return tickFormat;
-    tickFormat = x;
-    return score_bars;
-  };
-
-  return score_bars;
-};
-
-function get_score_pct(data, index) {
-  return data.score_pct;
-}
-
-function scoreBulletTranslate(x) {
-  return function(data) {
-    return "translate(" + x(data) + ",0)";
-  };
-}
-
-function scoreBulletWidth(x) {
-  var x0 = x(0);
-  return function(data) {
-    return Math.abs(x(data) - x0);
-  };
-}
-
+  function score_bullet_translate(x) {
+    return function(data) {
+      return "translate(" + x(data) + ",0)";
+    };
+  }
 })();
